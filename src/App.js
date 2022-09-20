d33d3908a9d07b60c0c8f1504bcb0944e1f7cc00
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-09-20 23:55:11 +0600

    Display countries in a simple way

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
 import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   return (
@@ -23,8 +23,20 @@ function LoadCountries(){
     <div>
       <h1>Visiting Every Countries of the world !!!</h1>
       <h3>Available Countries : {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   )
 }
+
+  function Country(props){
+    return(
+      <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population} </h4>
+      </div>
+    )
+  }
 
 export default App;
